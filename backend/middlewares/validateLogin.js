5f5719310a83cf7a4d02f544c7a9b91c65db66b6
Oxana Shovkoplyas <ekmati@yandex.ru>
2023-01-10 23:43:43 +0300

    vvv

--- backend/middlewares/validateLogin.js
+++ backend/middlewares/validateLogin.js
@@ -3,7 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 
 module.exports.validateLogin = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required(),
     password: Joi.string().required(),
   }).unknown(true),
 });
