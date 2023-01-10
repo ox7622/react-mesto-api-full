c1937e2e3b849ead09465d0ac8acabf861fee386
Oxana Shovkoplyas <ekmati@yandex.ru>
2023-01-10 23:18:15 +0300

    cors  pain

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const { errorLogger, requestLogger } = require('./middlewares/logger');
+const { cors } = require('./middlewares/cors');
 
 const { errorHandler } = require('./middlewares/errorHandler');
 const { validateLogin } = require('./middlewares/validateLogin');
@@ -20,6 +21,7 @@ mongoose.set('strictQuery', true);
 const { PORT = 3000, DB_LINK = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
+app.use(cors());
 
 // подключаемся к серверу mongo
 mongoose.connect(DB_LINK, {
@@ -32,37 +34,6 @@ app.use(express.json());
 app.use(requestLogger);
 
 app.use(express.static(path.join(__dirname, '../frontend'))); // теперь клиент имеет доступ только к публичным файлам
-
-// Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'https://ox7622.nomoredomains.club',
-  'http://ox7622.nomoredomains.club',
-  'localhost:3000',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-
-  next();
-});
 
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateCreateUser, createUser);
