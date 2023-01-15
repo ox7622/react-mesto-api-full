aac3c23c92c0f2fadc173c40c9897920c69ee941
Oxana Shovkoplyas <ekmati@yandex.ru>
2023-01-15 10:19:18 +0300

    fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,15 +8,17 @@ const allowedCors = [
 module.exports.cors = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
@@ -27,5 +29,5 @@ module.exports.cors = (req, res, next) => {
     return res.end();
   }
 
- // next();
+  next();
 };
