76b97bb9d0ae29a78439a944d88c0101a711613c
Oxana Shovkoplyas <ekmati@yandex.ru>
2023-01-17 18:02:19 +0300

    fix

--- backend/middlewares/checkToken.js
+++ backend/middlewares/checkToken.js
@@ -4,18 +4,21 @@ const LoginError = require('../errors/LoginError');
 const Error500 = require('../errors/Error500');
 
 module.exports.checkToken = (req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-    const authData = req.cookies.token;
-    if (!authData || authData === undefined) {
-      throw new LoginError('Пользователь не авторизован');
-    }
-    try {
-      jwt.verify(authData, process.env.TOKEN);
-    } catch (err) {
-      return next(new LoginError('Пользователь не авторизован'));
-    }
-    req.user = authData;
-    next();
+  // if (process.env.NODE_ENV === 'production') {
+  const authData = req.cookies.token;
+  if (!authData || authData === undefined) {
+    throw new LoginError('Пользователь не авторизован');
   }
-  return next(new Error500('нет кук'));
+  try {
+    const verified = jwt.verify(authData, process.env.TOKEN);
+    if (verified) {
+      req.user = authData;
+      console.log(req.user);
+      next();
+    }
+  } catch (err) {
+    return next(new LoginError('Пользователь не авторизован'));
+  }
+  //}
+  //return next(new Error500('нет кук'));
 };
